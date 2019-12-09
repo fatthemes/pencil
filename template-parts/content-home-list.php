54f189e0bc05a57c0f6a18e2f36553cdf0104d8c
patryk <patryk.kachel@limestreet.pl>
2019-12-09 16:46:16 +0100

    double image displaying issue fixed 2

--- template-parts/content-home-list.php
+++ template-parts/content-home-list.php
@@ -12,25 +12,37 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'col-md-12 masonry' ); ?>>
 	<header class="entry-header row">
 		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="featured-image col-sm-6">
-				<a href="<?php esc_url( the_permalink() ); ?>" rel="bookmark">
-				<?php the_post_thumbnail( 'medium' ); ?>   
-				</a>  
-				<?php echo wp_kses_post( pencil_post_format_icon( get_the_ID() ) ); ?>
-				<div class="featured-image-cat">
-				<?php the_category( __( ' &#x2f; ', 'pencil' ) ); ?>
+			<?php if (has_post_format( 'image' )) : ?>
+				<div class="featured-image col-sm-12">
+					<a href="<?php esc_url( the_permalink() ); ?>" rel="bookmark">
+					<?php the_post_thumbnail( 'large' ); ?>   
+					</a>  
+					<?php echo wp_kses_post( pencil_post_format_icon( get_the_ID() ) ); ?>
+					<div class="featured-image-cat">
+					<?php the_category( __( ' &#x2f; ', 'pencil' ) ); ?>
+					</div>
 				</div>
-			</div>
-			<div class="title-meta-wrapper col-sm-6">
-				<?php if ( has_post_format( 'link' ) || has_post_format( 'quote' ) || has_post_format( 'image' ) || has_post_format( 'aside' ) ) : ?>
-					<?php pencil_the_content(); ?>
-				<?php else : ?>
-					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-					<?php if ( 'post' == get_post_type() ) : ?>
-						<?php pencil_entry_meta(); ?>
+			<?php else : ?>
+				<div class="featured-image col-sm-6">
+					<a href="<?php esc_url( the_permalink() ); ?>" rel="bookmark">
+					<?php the_post_thumbnail( 'medium' ); ?>   
+					</a>  
+					<?php echo wp_kses_post( pencil_post_format_icon( get_the_ID() ) ); ?>
+					<div class="featured-image-cat">
+					<?php the_category( __( ' &#x2f; ', 'pencil' ) ); ?>
+					</div>
+				</div>
+				<div class="title-meta-wrapper col-sm-6">
+					<?php if ( has_post_format( 'link' ) || has_post_format( 'quote' ) || has_post_format( 'aside' ) ) : ?>
+						<?php pencil_the_content(); ?>
+					<?php else : ?>
+						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+						<?php if ( 'post' == get_post_type() ) : ?>
+							<?php pencil_entry_meta(); ?>
+						<?php endif; ?>
 					<?php endif; ?>
-				<?php endif; ?>
-			</div><!-- .title-meta-wrapper -->
+				</div><!-- .title-meta-wrapper -->
+			<?php endif; ?>
 		<?php else : ?>
 			<div class="title-meta-wrapper col-xs-12">
 				<?php echo wp_kses_post( pencil_post_format_icon( get_the_ID() ) ); ?>
